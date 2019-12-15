2c1a0f84c08d7b28f81287928cd35d7cc4aea8d4
Giuseppe Capizzi <g.capizzi@gmail.com>
2019-12-15 02:14:05 +0000

    Day 5

---
+++ day05/app/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import System.Environment
+import qualified Intcode
+import qualified Data.Text.IO as TIO
+import Data.Text (Text)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  sourceCode <- TIO.readFile $ head args
+  print $ Intcode.runWithIO (sourceCode, [5])
