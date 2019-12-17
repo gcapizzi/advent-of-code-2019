ab80878b8607d2459ceeb3f13a73856b3613f01f
Giuseppe Capizzi <g.capizzi@gmail.com>
2019-12-17 17:06:30 +0000

    Add an intcode CLI

---
+++ intcode/app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Intcode
+import System.Environment
+import qualified Data.Text.IO as TIO
+import Data.Text (Text)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  sourceCode <- TIO.readFile $ head args
+  let inputs = map read $ tail args
+  print $ Intcode.runWithIO (sourceCode, inputs)
